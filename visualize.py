a71c3d8255ab4bc08fc8ca3a479ab6c48de235ea
Fei-Tzin Lee <fl2301@columbia.edu>
2020-02-19 15:16:04 -0500

    more plots....

--- visualize.py
+++ visualize.py
@@ -317,7 +317,7 @@ if __name__ == "__main__":
     print(len(cues))
 
     #umap_plot(vectors, estimates, cues=cues, save_prefix='test')
-    for perplex in [50, 100, 200, 500, 1000, 2000]:
+    for perplex in [5, 10, 20]:#50, 100, 200, 500, 1000, 2000]:
         for lr in [50, 100, 500, 1000]:
             tsne(vectors, estimates, cues=cues, perplex=perplex, lr=lr, save_prefix='test-tsne-')
     
